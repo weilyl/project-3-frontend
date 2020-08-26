01d51c9dec472766f333df3a94712441c9c6a44a
emestiza <emestiza@u.rochester.edu>
2020-08-25 22:41:41 -0400

    Table & Chart

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 
 // table
-var data = [
+const data = [
   { "date" : '2013-01-01', "amount" : 45, "category" : "food" },
   { "date" : '2013-02-01', "amount" : 50, "category" : "food" },
   { "date" : '2013-03-01', "amount" : 55, "category" : "food" },
@@ -9,12 +9,12 @@ var data = [
   { "date" : '2013-06-01', "amount" : 50, "category" : "food" },
   { "date" : '2013-07-01', "amount" : 50, "category" : "transportation" },
   { "date" : '2013-08-01', "amount" : 52, "category" : "transportation" }
-]
+];
 		
 function tabulate(data, columns) {
-	var table = d3.select('body').append('table')
-	var thead = table.append('thead')
-	var	tbody = table.append('tbody');
+	const table = d3.select('body').append('table')
+	const thead = table.append('thead')
+	const	tbody = table.append('tbody');
 
 	// append the header row
 	thead.append('tr')
@@ -24,13 +24,13 @@ function tabulate(data, columns) {
 	    .text(function (column) { return column; });
 
 	// create a row for each object in the data
-	var rows = tbody.selectAll('tr')
+	const rows = tbody.selectAll('tr')
 	  .data(data)
 	  .enter()
 	  .append('tr');
 
 	// create a cell in each row for each column
-	var cells = rows.selectAll('td')
+	const cells = rows.selectAll('td')
 	  .data(function (row) {
 	    return columns.map(function (column) {
 	      return {column: column, value: row[column]};
@@ -46,18 +46,18 @@ function tabulate(data, columns) {
 // render the tables
 tabulate(data, ['date', 'amount', 'category']); // 3 column table
 
-var data_grouped = 
+const data_grouped = 
   { 
   
-}
+};
 
 // populate data_grouped array 
-for (let i=0; i<data.length; i++) {
+for (let i = 0; i < data.length; i++) {
   // check if category exits in grouped array
   // if it exists add amount
   // if it doesn't exist create new object and set the total to that amount
   
-  if (data_grouped[data[i].category])  {
+  if (data_grouped[data[i].category]) {
     // category total in data grouped += expense amount in data
     data_grouped[data[i].category] += data[i].amount
   } 
@@ -65,33 +65,33 @@ for (let i=0; i<data.length; i++) {
     // category name in data grouped object = expense amount
     data_grouped[data[i].category] = data[i].amount
   }
-}
+};
 
 
-var data_grouped_array = Object.entries(data_grouped).map((e) => ( { category:e[0], amount:e[1] } ));
+const data_grouped_array = Object.entries(data_grouped).map((e) => ( { category:e[0], amount:e[1] } ));
 
 
 // pie chart
-var w = 600,                            //width
-    h = 600,                            //height
-    r = 200,                            //radius
-    color = d3.scale.category20c();     //builtin range of colors
+const w = 600,                            //width
+      h = 600,                            //height
+      r = 200,                            //radius
+      color = d3.scale.category20c();     //builtin range of colors
     
-    var vis = d3.select("body")
+    const vis = d3.select("body")
         .append("svg:svg")              //create the SVG element inside the <body>
         .data([data_grouped_array])     //associate our data with the document
         .attr("width", w)               //set the width and height of our visualization (these will be attributes of the <svg> tag
         .attr("height", h)
-        .append("svg:g")                                      //make a group to hold our pie chart
-        .attr("transform", "translate(" + r + "," + r + ")")  //move the center of the pie chart from 0, 0 to radius, radius
+        .append("svg:g")                                       //make a group to hold our pie chart
+        .attr("transform", "translate(" + r + "," + r + ")");  //move the center of the pie chart from 0, 0 to radius, radius
 
-    var arc = d3.svg.arc()                                    //this will create <path> elements for us using arc data
+    const arc = d3.svg.arc()                                    //this will create <path> elements for us using arc data
         .outerRadius(r);
 
-    var pie = d3.layout.pie()                                 //this will create arc data for us given a list of values
-        .value(function(d) { return d.amount; });             //we must tell it out to access the value of each element in grouped data array
+    const pie = d3.layout.pie()                                 //this will create arc data for us given a list of values
+        .value(function(d) { return d.amount; });               //we must tell it out to access the value of each element in grouped data array
 
-    var arcs = vis.selectAll("g.slice")     //this selects all <g> elements with class slice (there aren't any yet)
+    const arcs = vis.selectAll("g.slice")   //this selects all <g> elements with class slice (there aren't any yet)
         .data(pie)                          //associate the generated pie data (an array of arcs, each having startAngle, endAngle and value properties) 
         .enter()                            //this will create <g> elements for every "extra" data element that should be associated with a selection. The result is creating a <g> for every object in the data array
         .append("svg:g")                    //create a group to hold each slice (we will have a <path> and a <text> element associated with each slice)
