d2e6b84a78aa8ef3f369fdb6113848b388052575
weilyl <64621097+weilyl@users.noreply.github.com>
2020-08-26 21:23:57 -0400

    Revert "Merge pull request #25 from weilyl/eric/table"
    This reverts commit d5ac361c95b2d17be979fec1b806d6dc5684f518, reversing
    changes made to c3f2faa1fb68a637a792ed86fd512cbc4963a6c6.

--- js/app.js
+++ js/app.js
@@ -1,69 +1,47 @@
 // table
 let data = [
-  { "id" : 1, "date" : '2013-01-01', "amount" : 45, "category" : "food" },
-  { "id" : 2,"date" : '2013-02-01', "amount" : 50, "category" : "food" },
-  { "id" : 3,"date" : '2013-03-01', "amount" : 55, "category" : "food" },
-  { "id" : 4,"date" : '2013-04-01', "amount" : 50, "category" : "food" },
-  { "id" : 5,"date" : '2013-05-01', "amount" : 45, "category" : "transportation" },
-  { "id" : 6,"date" : '2013-06-01', "amount" : 50, "category" : "transportation" },
-  { "id" : 7,"date" : '2013-07-01', "amount" : 50, "category" : "transportation" },
-  { "id" : 8,"date" : '2013-08-01', "amount" : 52, "category" : "transportation" }
+  { date: "2013-01-01", amount: 45, category: "food" },
+  { date: "2013-02-01", amount: 50, category: "food" },
+  { date: "2013-03-01", amount: 55, category: "food" },
+  { date: "2013-04-01", amount: 50, category: "food" },
+  { date: "2013-05-01", amount: 45, category: "transportation" },
+  { date: "2013-06-01", amount: 50, category: "transportation" },
+  { date: "2013-07-01", amount: 50, category: "transportation" },
+  { date: "2013-08-01", amount: 52, category: "transportation" },
 ];
 
 function tabulate(data, columns) {
-	let table = d3.select('body').append('table')
-	let thead = table.append('thead')
-  let	tbody = table.append('tbody');
+  let table = d3.select("body").append("table");
+  let thead = table.append("thead");
+  let tbody = table.append("tbody");
 
-	// append the header row
-	thead.append('tr')
-	  .selectAll('th')
-	  .data(columns).enter()
-	  .append('th')
-	    .text(function (column) { return column; });
+  // append the header row
+  thead
+    .append("tr")
+    .selectAll("th")
+    .data(columns)
+    .enter()
+    .append("th")
+    .text(function (column) {
+      return column;
+    });
 
-	// create a row for each object in the data
-	let rows = tbody.selectAll('tr')
-	  .data(data)
-	  .enter()
-    .append('tr');
-  console.log(rows);
-  console.table(rows);
+  // create a row for each object in the data
+  let rows = tbody.selectAll("tr").data(data).enter().append("tr");
 
-	// create a cell in each row for each column
-	let cells = rows.selectAll('td')
-	  .data(function (row) {
-	    return columns.map(function (column) {
-	      return {column: column, value: row[column]};
-	    });
-	  })
-	  .enter()
-	  .append('td')
-      .text(function (d) { return d.value; });
-      
-  // update button
-  thead.append("th").text('update');
-  rows.selectAll("td.update")  
-  // use a class so you don't re-select the existing <td> elements
-          .data(function(d) {return [d];})
-          .enter()
-            .append("td")
-            .attr("id", `${d.id}`)
-            .append("button")
-            .text(function(d){return "update"})
-            .on("click", function(d){ console.log(d); alert("hello")});
-
-  // delete button
-  thead.append("th").text('delete');
-  rows.selectAll("td.delete")  
-  // use a class so you don't re-select the existing <td> elements
-          .data(function(d) {return [d];})
-          .enter()
-            .append("td")
-            .attr("id", `${data.id}`)
-            .append("button")
-            .text(function(d){return "delete"})
-            .on("click", function(d){ console.log(d); alert("hello")});
+  // create a cell in each row for each column
+  let cells = rows
+    .selectAll("td")
+    .data(function (row) {
+      return columns.map(function (column) {
+        return { column: column, value: row[column] };
+      });
+    })
+    .enter()
+    .append("td")
+    .text(function (d) {
+      return d.value;
+    });
 
   return table;
 }
