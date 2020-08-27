e9ccfe5d077700a0f4cb0e48eaf511343c182d26
weilyl <weily.lang@gmail.com>
2020-08-26 21:27:16 -0400

    trying to make git happy

--- js/login.js
+++ js/login.js
@@ -1,62 +1,39 @@
 const login = new Vue({
-  el: "#login",
-  data: {
-    loggedin: false,
-    JWT: "",
-    createUN: "",
-    createPW: "",
-    loginUN: "",
-    loginPW: "",
-    devURL: "http://localhost:3000",
-    prodURL: "https://squilliamp3.herokuapp.com",
-    user: null,
-    token: null,
-  },
-  methods: {
-    handleLogin: function (event) {
-      event.preventDefault();
-      const URL = this.prodURL ? this.prodURL : this.devURL;
-      const user = { username: this.loginUN, password: this.loginPW };
-      fetch(`${URL}/login`, {
-        method: "post",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(user),
-      })
-        .then((response) => response.json())
-        .then((data) => {
-          this.user = data.user;
-          this.token = data.token;
-          this.loggedin = true;
-          budget.loggedin = true;
-          expense.loggedin = true;
-          this.loginPW = "";
-          this.loginUN = "";
-          if (data.error) {
-            alert("log in unsuccessful");
-          } else {
-            alert("log in successful");
-          }
-        });
+    el: "#login",
+    data: {
+        loggedin: false,
+        JWT: "",
+        createUN: "",
+        createPW: "",
+        loginUN: "",
+        loginPW: "",
+        devURL: "http://localhost:3000",
+        prodURL: "https://squilliamp3.herokuapp.com",
+        user: null,
+        token: null
     },
-    handleLogout: function () {
-      this.loggedin = false;
-      this.user = null;
-      this.token = null;
-    },
-    handleSignup: function () {
-      const URL = this.prodURL ? this.prodURL : this.devURL;
-      const user = JSON.stringify({
-        username: this.createUN,
-        password: this.createPW,
-      });
-
-      fetch(`${URL}/users`, {
-        method: "post",
-        headers: {
-          "Content-Type": "application/json",
+    methods: {
+        handleLogin: function() {
+            const URL = this.prodURL ? this.prodURL : this.devURL;
+            const user = {username: this.loginUN, password: this.loginPW};
+            console.log("hello");
+            fetch(`${URL}/login`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: user
+            })
+                .then((response) => response.json())
+            .then((data) => {
+                this.user = data.user;
+                this.token = data.token;
+                this.loggedin = true;
+                this.loginPW = ""
+                this.loginUN = ""
+            });
         },
+<<<<<<< HEAD
         body: user,
       })
         .then((response) => response.json())
@@ -71,10 +48,38 @@ const login = new Vue({
     },
   },
 });
+=======
+        handleLogout:function () {
+            this.loggedin = false;
+            this.user = null;
+            this.token = null;
+        },
+        handleSignup: function () {
+            const URL = this.prodURL ? this.prodURL : this.devURL;
+            console.log(URL);
+            const user = JSON.stringify({
+                username: this.createUN,
+                password: this.createPW,
+            });
+            console.log(user);
+>>>>>>> parent of 0a0f2c1... Table & Chart
 
-const heading = new Vue ({
-  el: "#heading",
-  data: {
-    heading: `${login.loginUN}'s ${budget.budName} Budget`
-  }
+            fetch(`${URL}/users`, {
+                method: "post",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: user
+            })
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data)
+                if(data.error) {
+                    alert("sign up unsuccessful");
+                } else {
+                    alert("signup successful")
+                }
+            });
+        }
+    }
 })
