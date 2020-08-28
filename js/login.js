e0c45c55cd264fa49265aa1a7059199cf290b4cf
Marilyn Balkaransingh <marilynbalkaransingh@marilyns-air.home>
2020-08-28 12:36:20 -0400

    unknown login change

--- js/login.js
+++ js/login.js
@@ -15,10 +15,77 @@ const login = new Vue({
   },
   methods: {
     handleLogin: function () {
-      //event.preventDefault();
       const URL = this.prodURL ? this.prodURL : this.devURL;
       const user = { username: this.loginUN, password: this.loginPW };
       fetch(`${URL}/login`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(user),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          this.user = data.user;
+          this.token = data.token;
+          this.loggedin = true;
+          this.loginPW = "";
+          this.loginUN = "";
+          this.user_id = data.user.id;
+        });
+    },
+    // handleLogin: function () {
+    //   //event.preventDefault();
+    //   const URL = this.prodURL ? this.prodURL : this.devURL;
+    //   const user = { username: this.loginUN, password: this.loginPW };
+    //   fetch(`${URL}/login`, {
+    //     method: "post",
+    //     headers: {
+    //       "Content-Type": "application/json",
+    //     },
+    //     body: JSON.stringify(user),
+    //   })
+    //     .then((response) => response.json())
+    //     .then((data) => {
+    //       if (data.error) {
+    //         alert("log in unsuccessful");
+    //       } else {
+    //         alert("log in successful");
+    //           this.token = data.token;
+    //           console.log("1 ish", this.token);
+    //           this.loggedin = true;
+    //           budget.loggedin = true;
+    //           expense.loggedin = true;
+    //           this.loginPW = "";
+    //           this.loginUN = "";
+    //           console.log("2 ish", this.token);
+    //           if (){
+    //               budget.createBudget();
+    //           }else {
+
+    //           }
+    //           return this.token;
+    //       }
+    //     })
+    //       .then(() => {
+    //           expense.showExpense();
+    //           return this.token;
+    //       });
+    // },
+    handleLogout: function () {
+      this.loggedin = false;
+      this.user = null;
+      this.token = null;
+    },
+    handleSignup: function () {
+      const URL = this.prodURL ? this.prodURL : this.devURL;
+      const user = JSON.stringify({
+        username: this.createUN,
+        password: this.createPW,
+      });
+      console.log(user);
+​
+      fetch(`${URL}/users`, {
         method: "post",
         headers: {
           "Content-Type": "application/json",
@@ -27,66 +94,44 @@ const login = new Vue({
       })
         .then((response) => response.json())
         .then((data) => {
+          console.log(data);
           if (data.error) {
-            alert("log in unsuccessful");
-          } else {
-            alert("log in successful");
-              this.token = data.token;
-              console.log("1 ish", this.token);
-              this.loggedin = true;
-              budget.loggedin = true;
-              expense.loggedin = true;
-              this.loginPW = "";
-              this.loginUN = "";
-              console.log("2 ish", this.token);
-              if (){
-                  budget.createBudget();
-              }else {
-
-              }
-              return this.token;
-          }
-        })
-          .then(() => {
-              expense.showExpense();
-              return this.token;
-          });
-    },
-    handleLogout: function () {
-      this.loggedin = false;
-      this.user = null;
-      this.token = null;
-    },
-     async handleSignup () {
-      const URL = this.prodURL ? this.prodURL : this.devURL;
-      const user = JSON.stringify({
-        username: this.createUN,
-        password: this.createPW,
-      });
-      const response = await fetch(`${URL}/users`, {
-        method: "post",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: user,
-      })
-      const data = await response.json();
-        if (data.error) {
             alert("sign up unsuccessful");
-        } else {
+          } else {
             alert("signup successful");
-            this.loginUN = this.createUN;
-            console.log(this.createUN)
-            console.log("becomes ", this.loginUN)
-            this.loginPW = this.createPW;
-            await this.handleLogin();
-            console.log("1: ", this.token)
-            this.user = data.user;
-            this.user_id = data.user.id;
-            console.log("2: ", this.token)
-            console.log("3: ", this.token)
-        }
+          }
+        });
     },
+    //  async handleSignup () {
+    //   const URL = this.prodURL ? this.prodURL : this.devURL;
+    //   const user = JSON.stringify({
+    //     username: this.createUN,
+    //     password: this.createPW,
+    //   });
+    //   const response = await fetch(`${URL}/users`, {
+    //     method: "post",
+    //     headers: {
+    //       "Content-Type": "application/json",
+    //     },
+    //     body: user,
+    //   })
+    //   const data = await response.json();
+    //     if (data.error) {
+    //         alert("sign up unsuccessful");
+    //     } else {
+    //         alert("signup successful");
+    //         this.loginUN = this.createUN;
+    //         console.log(this.createUN)
+    //         console.log("becomes ", this.loginUN)
+    //         this.loginPW = this.createPW;
+    //         await this.handleLogin();
+    //         console.log("1: ", this.token)
+    //         this.user = data.user;
+    //         this.user_id = data.user.id;
+    //         console.log("2: ", this.token)
+    //         console.log("3: ", this.token)
+    //     }
+    // },
   },
 });
 
