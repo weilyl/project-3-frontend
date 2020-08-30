85657c05002a33a285e18efb57d9fe8e768620cf
emestiza <emestiza@u.rochester.edu>
2020-08-30 15:54:59 -0400

    Updated files and responsive design

--- js/login.js
+++ js/login.js
@@ -99,7 +99,6 @@ const login = new Vue({
       })
         .then((response) => response.json())
         .then((data) => {
-          console.log(data);
           if (data.error) {
             alert("sign up unsuccessful");
           } else {
